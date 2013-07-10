01fa624fa6a6138539d62bf04884877338711a36
caninemwenja <mwenja07@gmail.com>
2013-07-10 19:43:54 +0300

    added location to lat,long function

---
+++ time_functions.py
@@ -0,0 +1,15 @@
+import pytz
+import datetime
+import urllib2
+import json
+
+def gps_from_location(location):
+    """ Returns a tuple with latitude and longitude of the location provided """
+
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false"
+    data = urllib2.urlopen(url % urllib2.quote(location))
+    json_string = data.read()
+    json_obj = json.loads(json_string)
+    lat = json_obj['results'][0]['geometry']['location']['lat']
+    long = json_obj['results'][0]['geometry']['location']['lng']
+    return "%s,%s" % (lat, long)
