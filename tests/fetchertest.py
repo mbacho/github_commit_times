14ae6585ef9b1bdfdbc1dada47f3374c2b37cc7f
caninemwenja <mwenja07@gmail.com>
2013-08-16 16:27:58 +0300

    added assertions

--- tests/fetchertest.py
+++ tests/fetchertest.py
@@ -51,6 +51,41 @@ class FetcherTest(unittest.TestCase):
 		self.assertEqual(result[0]["language"], None, "Langauge does not match, Language: "+ repr(result[0]["language"]))
 		self.assertEqual(result[0]["created"], "2011-01-27T19:30:43Z", "Created does not match, Created: "+ repr(result[0]["created"]))
 
+	def test_process_repo_multiple_repo(self):
+		self.fetcher.get_from_net = Mock(return_value=self.get_data_file("octocat.json"))
+		result = self.fetcher.process_repo("", True)
+
+		self.assertIsInstance(result, type(list()), "Result was not a list")
+		self.assertNotEqual(len(result), 0, "List is empty")
+		self.assertEqual(len(result), 3, "List has extra items")
+		self.assertIsInstance(result[0], type(dict()), "List item is not a dictionary: "+repr(result[0]))
+		self.assertIsInstance(result[1], type(dict()), "List item is not a dictionary: "+repr(result[0]))
+		self.assertIsInstance(result[2], type(dict()), "List item is not a dictionary: "+repr(result[0]))
+
+		# testing membership - first item
+		self.assertIn("full_name", result[0], "Full name missing from dictionary: "+ repr(result[0]))
+		self.assertIn("name", result[0], "Name missing from dictionary: "+ repr(result[0]))
+		self.assertIn("fork", result[0], "Fork missing from dictionary: "+ repr(result[0]))
+		self.assertIn("url", result[0], "URL missing from dictionary: "+ repr(result[0]))
+		self.assertIn("language", result[0], "Language missing from dictionary: "+ repr(result[0]))
+		self.assertIn("created", result[0], "Created missing from dictionary: "+ repr(result[0]))
+
+		# testing membership - second item
+		self.assertIn("full_name", result[1], "Full name missing from dictionary: "+ repr(result[1]))
+		self.assertIn("name", result[1], "Name missing from dictionary: "+ repr(result[1]))
+		self.assertIn("fork", result[1], "Fork missing from dictionary: "+ repr(result[1]))
+		self.assertIn("url", result[1], "URL missing from dictionary: "+ repr(result[1]))
+		self.assertIn("language", result[1], "Language missing from dictionary: "+ repr(result[1]))
+		self.assertIn("created", result[1], "Created missing from dictionary: "+ repr(result[1]))
+
+		# testing values - third item
+		self.assertEqual(result[2]["full_name"], "octocat/ThisIsATest", "Full name does not match, Fullname: "+ repr(result[2]["full_name"]))
+		self.assertEqual(result[2]["name"], "ThisIsATest", "Name does not match, Name: "+ repr(result[2]["name"]))
+		self.assertEqual(result[2]["fork"], False, "Fork does not match, Fork: "+ repr(result[2]["fork"]))
+		self.assertEqual(result[2]["url"], "https://api.github.com/repos/octocat/ThisIsATest", "URL does not match, URL: "+ repr(result[2]["url"]))
+		self.assertEqual(result[2]["language"], None, "Langauge does not match, Language: "+ repr(result[2]["language"]))
+		self.assertEqual(result[2]["created"], "2012-03-07T23:25:47Z", "Created does not match, Created: "+ repr(result[2]["created"]))
+
 	def tearDown(self):
 		self.fetcher = None
 
