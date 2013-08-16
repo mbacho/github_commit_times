fe8863d955b2fae1c6c40ee2287af6b66d6c8798
caninemwenja <mwenja07@gmail.com>
2013-08-16 13:28:21 +0300

    some housekeeping

--- tests/fetchertest.py
+++ tests/fetchertest.py
@@ -29,11 +29,13 @@ class FetcherTest(unittest.TestCase):
 	def test_process_repo_single_repo(self):
 		self.fetcher.get_from_net = Mock(return_value=self.get_data_file("octocat.Spoon-Knife.json"))
 		result = self.fetcher.process_repo("")
+
 		self.assertIsInstance(result, type(list()), "Result was not a list")
 		self.assertNotEqual(len(result), 0, "List is empty")
 		self.assertEqual(len(result), 1, "List has extra items")
 		self.assertIsInstance(result[0], type(dict()), "List item is not a dictionary: "+repr(result[0]))
 
+		# testing membership
 		self.assertIn("full_name", result[0], "Full name missing from dictionary: "+ repr(result[0]))
 		self.assertIn("name", result[0], "Name missing from dictionary: "+ repr(result[0]))
 		self.assertIn("fork", result[0], "Fork missing from dictionary: "+ repr(result[0]))
@@ -41,13 +43,13 @@ class FetcherTest(unittest.TestCase):
 		self.assertIn("language", result[0], "Language missing from dictionary: "+ repr(result[0]))
 		self.assertIn("created", result[0], "Created missing from dictionary: "+ repr(result[0]))
 
+		# testing values
 		self.assertEqual(result[0]["full_name"], "octocat/Spoon-Knife", "Full name does not match, Fullname: "+ repr(result[0]["full_name"]))
 		self.assertEqual(result[0]["name"], "Spoon-Knife", "Name does not match, Name: "+ repr(result[0]["name"]))
 		self.assertEqual(result[0]["fork"], False, "Fork does not match, Fork: "+ repr(result[0]["fork"]))
 		self.assertEqual(result[0]["url"], "https://api.github.com/repos/octocat/Spoon-Knife", "URL does not match, URL: "+ repr(result[0]["url"]))
 		self.assertEqual(result[0]["language"], None, "Langauge does not match, Language: "+ repr(result[0]["language"]))
 		self.assertEqual(result[0]["created"], "2011-01-27T19:30:43Z", "Created does not match, Created: "+ repr(result[0]["created"]))
-
 
 	def tearDown(self):
 		self.fetcher = None
