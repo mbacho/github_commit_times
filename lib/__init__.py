cb456d009fd98cf7d661307fcd6cb5bf5c752e3f
Chomba Ng'ang'a <choeringa@gmail.com>
2013-08-18 17:06:08 +0300

    Separating fetching of public repos from pulling specific repos and user repos

--- lib/__init__.py
+++ lib/__init__.py
@@ -8,13 +8,14 @@ Library for this project
 ## URLs used in this library
 BASE_URL = "https://api.github.com/"
 
-
 USER_URL = "users/{user}"
 USER_REPO_LIST = "users/{user}/repos"
 
 REPO_URL = "repos/{full_name}"
-ALL_REPO_LIST = "repositories"
+ALL_REPO_LIST = "repositories?since={since}" #since -> The integer ID of the last repository that you've seen
+DEFAULT_MAX_PUBLIC_REPOS =  100 #max number of repos to be taken by default
 
 REPO_COMMIT_LIST = "repos/{full_name}/commits" #"/repos/{owner}/{repo}/commits"
 RAW_COMMIT = "{commit_href}.patch"
 COMMIT_DETAILS = "repos/{full_name}/commits/{commit_sha}"
+
